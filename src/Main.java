9806d17552c7b00db76f59af9ed5b57124cf766c
Iskhak-69 <iskhak.koichubaev@alatoo.edu.kg>
2023-12-05 08:47:01 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> playerNames = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        boolean game = true;
+        while(game){
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+        }
+    }
+}
