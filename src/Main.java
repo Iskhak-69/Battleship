755b2cbede4fb7491c4eb050dceec14531b27cd8
Iskhak-69 <iskhak.koichubaev@alatoo.edu.kg>
2023-12-05 17:45:31 +0600

    rearrange

--- src/Main.java
+++ src/Main.java
@@ -28,20 +28,22 @@ public class Main {
                 while(isTrue){
                     int x = random.nextInt(1, 8);
                     int y = random.nextInt(1, 8);
+                    System.out.println(x);
+                    System.out.println(y);
                     int direction = random.nextInt(1,3);
                     //direction == 1 is horizontal
                     //direction == 2 is vertical
                     if(direction == 1){
                         if(y > 1 && y < 7){
-                            seaField[x][y] = '#';
-                            seaField[x][y - 1] = '#';
-                            seaField[x][y + 1] = '#';
+                            seaField[x - 1][y - 1] = '#';
+                            seaField[x - 1][y - 2] = '#';
+                            seaField[x - 1][y] = '#';
                             isTrue = false;
                         }
                         else if(x > 1 && x < 7){
-                            seaField[x][y] = '#';
-                            seaField[x - 1][y] = '#';
-                            seaField[x + 1][y] = '#';
+                            seaField[x - 1][y - 1] = '#';
+                            seaField[x - 2][y - 1] = '#';
+                            seaField[x][y - 1] = '#';
                             isTrue = false;
                         }
                     }
---
+++ src/test.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class test {
+    static Random random = new Random();
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[][] seaField = getField();
+    }
+
+    public static void threeDeckShip(char[][] seaField){
+        boolean isShipPlaced = false;
+
+        while(!isShipPlaced){
+            int i = random.nextInt(1, 8);
+            int j = random.nextInt(1, 8);
+            int direction = random.nextInt(1, 3);
+
+            if(direction == 1){
+                if(canThreeDeckShipPlace(i, j, direction)){
+                    seaField[i][j] = '#';
+                    seaField[i][j + 1] = '#';
+                    seaField[i][j + 2] = '#';
+
+                }
+            }else{
+                if(canThreeDeckShipPlace(i, j, direction)){
+                    seaField[i][j] = '#';
+                    seaField[i + 1][j] = '#';
+                    seaField[i + 2][j] = '#';
+                }
+            }
+        }
+    }
+
+    public static boolean canThreeDeckShipPlace(int i, int j, int direction){
+        if(direction == 1){
+            if(j > 4){
+                return false;
+            }
+        }else{
+            if(i > 4){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void occupyThreeDeckShip(int i, int j){
+
+    }
+
+    public static char[][] getField(){
+        char[][] seaField = new char[][]{};
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                seaField[i][j] = '0';
+            }
+        }
+        return seaField;
+    }
+}
