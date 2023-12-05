86931ea19103106bc230379e8a7ced1527a4a282
Iskhak-69 <iskhak.koichubaev@alatoo.edu.kg>
2023-12-05 09:38:56 +0600

    three deck ship added

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,54 @@ public class Main {
         while(game){
             System.out.println("Enter your name: ");
             String name = scanner.nextLine();
-
+            battleship();
         }
+    }
+    public static char[][] battleship(){
+        Random random = new Random();
+        int fieldSize = 7;
+        int oneDeckShip = 4;
+        int twoDeckShip = 2;
+        int threeDeckShip = 1;
+        char[][] seaField = new char[fieldSize][fieldSize];
+        for (int i = 0; i < fieldSize; i++) {
+            for (int j = 0; j < fieldSize; j++) {
+                seaField[i][j] = '*';
+            }
+        }
+        while(threeDeckShip > 0){
+            if(threeDeckShip > 0){
+                boolean isTrue = true;
+                while(isTrue){
+                    int x = random.nextInt(1, 8);
+                    int y = random.nextInt(1, 8);
+                    int direction = random.nextInt(1,3);
+                    //direction == 1 is horizontal
+                    //direction == 2 is vertical
+                    if(direction == 1){
+                        if(y > 1 && y < 7){
+                            seaField[x][y] = '#';
+                            seaField[x][y - 1] = '#';
+                            seaField[x][y + 1] = '#';
+                            isTrue = false;
+                        }
+                        else if(x > 1 && x < 7){
+                            seaField[x][y] = '#';
+                            seaField[x - 1][y] = '#';
+                            seaField[x + 1][y] = '#';
+                            isTrue = false;
+                        }
+                    }
+                }
+            }
+            threeDeckShip--;
+        }
+        for (int i = 0; i < fieldSize; i++) {
+            for (int j = 0; j < fieldSize; j++) {
+                System.out.print(seaField[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return seaField;
     }
 }
