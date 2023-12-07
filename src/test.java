f991cc7d3b2e655a64bebe78b9a5abe8699fd34e
Iskhak-69 <iskhak.koichubaev@alatoo.edu.kg>
2023-12-07 17:46:18 +0600

    added regions

--- src/test.java
+++ src/test.java
@@ -19,13 +19,14 @@ public class test {
         }
     }
 
+    //region threeDeckShip
     public static void threeDeckShip(char[][] seaField){
         boolean isShipPlaced = false;
 
         while(!isShipPlaced){
             int i = random.nextInt(0, 7);
             int j = random.nextInt(0, 7);
-            int direction = random.nextInt(1, 2);
+            int direction = random.nextInt(1, 3);
 //            int i = 2;
 //            int j = 5;
 //            int direction = 2;
@@ -102,6 +103,95 @@ public class test {
 
         }
     }
+    //endregion
+
+    //region twoDeckShip
+    public static void twoDeckShip(char[][] seaField){
+        boolean isShipPlaced = false;
+
+        while(!isShipPlaced){
+            int i = random.nextInt(0, 7);
+            int j = random.nextInt(0, 7);
+            int direction = random.nextInt(1, 3);
+//            int i = 2;
+//            int j = 5;
+//            int direction = 2;
+
+            if(direction == 1){
+                if(canThreeDeckShipPlace(i, j, direction)){
+                    System.out.println(i + " " + j);
+                    seaField[i][j] = '#';
+                    seaField[i][j + 1] = '#';
+                    seaField[i][j + 2] = '#';
+
+                    isShipPlaced = true;
+                    occupyThreeDeckShip(i, j, seaField, direction);
+                }
+            }else{
+                if(canThreeDeckShipPlace(i, j, direction)){
+                    seaField[i][j] = '#';
+                    seaField[i + 1][j] = '#';
+                    seaField[i + 2][j] = '#';
+
+                    isShipPlaced = true;
+                    occupyThreeDeckShip(i, j, seaField, direction);
+
+                }
+            }
+        }
+
+    }
+
+    public static boolean canTwoDeckShipPlace(int i, int j, int direction){
+        if(direction == 1){
+            if(j > 4){
+                return false;
+            }
+        }else{
+            if(i > 4){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void occupyTwoDeckShip(int i, int j, char[][] seaField, int direction){
+        if(direction == 1){
+            for(int counterI = i - 1; counterI < i + 2; counterI++){
+                for(int counterJ = j - 1; counterJ < j + 4; counterJ++){
+                    if((counterI == i && counterJ == j) || (counterI == i && counterJ == j + 1) || (counterI == i && counterJ == j + 2)){
+                        continue;
+                    }
+
+                    if((counterI < 0 || counterI > 6) || (counterJ < 0 || counterJ > 6)){
+                        continue;
+                    }
+
+                    seaField[counterI][counterJ] = '1';
+                }
+            }
+        }else{
+
+            for(int counterI = i - 1; counterI < i + 4; counterI++){
+                for(int counterJ = j - 1; counterJ < j + 2; counterJ++){
+                    if((counterI == i && counterJ == j) || (counterI == i + 1 && counterJ == j) || (counterI == i + 2 && counterJ == j)){
+                        continue;
+                    }
+
+                    if((counterI < 0 || counterI > 6) || (counterJ < 0 || counterJ > 6)){
+                        continue;
+                    }
+
+                    seaField[counterI][counterJ] = '1';
+                }
+            }
+
+        }
+    }
+    //endregion
+
+
 
     public static char[][] getField(){
         char[][] seaField = new char[7][7];
